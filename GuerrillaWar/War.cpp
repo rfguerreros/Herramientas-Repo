fc52b0f815de65d39d0d84fe495051d256d66439
Diego Veloza Diaz <dvelozad@unal.edu.co>
2016-08-15 17:50:39 -0500

    Agregados los numeros aleatorios

--- GuerrillaWar/War.cpp
+++ GuerrillaWar/War.cpp
@@ -1,9 +1,15 @@
 #include<iostream>
 #include<vector>
+#include<random>
+#include<ctime>
+
 
 using namespace std;
 
 int main(){
+
+  mt19937 randomGen(time(0));
+  uniform_int_distribution <int> diceRoll (1, 100);
   
   int NumberUnits = 10000;
   double FragmentationProb = 0.01;
@@ -12,7 +18,7 @@ int main(){
   R.resize(NumberUnits);
   R[0]=NumberUnits;
 
-  
+  cout << diceRoll(randomGen)  << endl;
   
   return 0;
 }
