81add8d597819fa3fa231b60415afe74014c7751
Diego Veloza Diaz <dvelozad@unal.edu.co>
2016-08-15 17:20:32 -0500

    Primer codigo, Funciona

---
+++ GuerrillaWar/War.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(){
+  
+  int NumberUnits = 10000;
+  double FragmentationProb = 0.01;
+
+  vector <int> R;
+  R.resize(NumberUnits);
+  R[0]=NumberUnits;
+
+  
+  
+  return 0;
+}
