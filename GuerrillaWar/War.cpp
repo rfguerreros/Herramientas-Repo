d53746652aba312fe688d9a61169e64c013b01f3
Diego Veloza Diaz <dvelozad@unal.edu.co>
2016-08-16 19:45:13 -0500

    Map working, each 1 takes a single number

--- GuerrillaWar/War.cpp
+++ GuerrillaWar/War.cpp
@@ -4,69 +4,39 @@
 #include<vector>
 #include<random>
 #include<ctime>
-
+#include<map>
 
 using namespace std;
 
 int main(){
 
-  long Y;
-  long Z;
-  long X;
-
-  long S=0;
-  
   long NumberUnits = 1000;
-  
+ 
   
   mt19937 randomGen(time(0));
   uniform_int_distribution <int> diceRoll (1, 100);
   uniform_int_distribution <int> randI (0,NumberUnits);
-  uniform_int_distribution <int> randJ(0,NumberUnits);
   
+
+  map<int,int> Map;
   
-  vector <long> R;
-  R.resize(NumberUnits);
+  vector <long> Units;
+
+  Units.resize(NumberUnits);
+  
+  for(int i = 0; i < NumberUnits; i++){
+    Units[i]=1;
+  }
 
   for(int i = 0; i < NumberUnits; i++){
-    R[i]=1;
-  }
-
-  for(int i = 0; i < 100000; i++){
-    Y=randI(randomGen);
-    
-
-    X=diceRoll(randomGen);
-    
-    if(X!=50){
-
-      Z=randJ(randomGen);
-      
-      R[Y]=R[Y]+R[Z];
-      R[Z]=0;
-      
-    } else if(X==50 && R[Y]>1){
-	for(int k = 0; k < NumberUnits; k++){
-	  if(R[k]==0){
-	    R[k]=1;
-	    S++;
-	  }
-	  if(S == R[Y]) break;
-	}
-	R[Y]=1;
+    for(int j = 0; j < Units[i]; j++){
+      Map.insert(pair<int,int>(i,j));
     }
   }
-
-  int c=0;
-  for(int i = 0; i < NumberUnits; i++){
-    if(R[i]>1){
-    cout<<R[i]<<"-";
-    }
-    c=c+R[i];
-  }
-
-  cout << "\n" <<c << endl;
   
+
+  
+  cout << Map.count(500) << endl;
   
   return 0;
 }
