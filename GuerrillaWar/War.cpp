5ecb4d9a4a557c4ab5976c7d9e095567c6da0d21
Diego Veloza Diaz <dvelozad@unal.edu.co>
2016-08-16 20:24:16 -0500

    The map is working, from here we'll implement the fragmentation/Union

--- GuerrillaWar/War.cpp
+++ GuerrillaWar/War.cpp
@@ -11,7 +11,9 @@ using namespace std;
 int main(){
 
   long NumberUnits = 1000;
- 
+
+  long Counter = 0;
+  long C=0;
   
   mt19937 randomGen(time(0));
   uniform_int_distribution <int> diceRoll (1, 100);
@@ -27,16 +29,24 @@ int main(){
   for(int i = 0; i < NumberUnits; i++){
     Units[i]=1;
   }
-
+  
+  Units[0]=0;
+  Units[1]=0;
+  Units[2]=3;
+  
   for(int i = 0; i < NumberUnits; i++){
+    if (Units[i]==0) continue;
     for(int j = 0; j < Units[i]; j++){
-      Map.insert(pair<int,int>(i,j));
+      Map.insert(pair<long,long>(C+j,Units[i]));
+      Counter++;
     }
+    C=Counter;
+  
   }
   
 
   
-  cout << Map.count(500) << endl;
+  cout << Map[0] <<" "<< Map[1]<<" "<< Map[3]<< endl;
   
   return 0;
 }
